d508b024cfd8f7ceaa05fc301eb28ce2de0d2842
Ramees Mohammed M M <70532769+7h3cyb3rm0nk@users.noreply.github.com>
2025-10-07 17:38:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,10 @@ const App = () => {
       <UserProfile name="Ramees" image="/images/ramees.jpg" >
         Msc AI 1st year
           </UserProfile>
+         
+      <UserProfile name="Ramees" image="/images/ramees.jpg" >
+        Msc AI 1st year
+          </UserProfile>
 
         <UserProfile name="GhanShyam" image="/images/ramees.jpg" >
       <UserProfile name="Billy" image="/images/ramees2.jpeg" >
