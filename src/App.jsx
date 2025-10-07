9ed27476deb5aee7392fd888ee40d49a9ea94fc9
Jeswin Tom <jeswintom8@gmail.com>
2025-10-07 17:43:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,9 @@ const App = () => {
         Msc AI 1st year
           </UserProfile>
 
-          
+             <UserProfile name="Jeswin" image="/images/ramees2.jpeg" >
+        Msc AI 1st year
+          </UserProfile>
                 
  <UserProfile name="Aditya R Nair" image="/images/Aditya at IITMM.jpg" >
         Integrated MCA
